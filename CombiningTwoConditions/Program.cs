ac9192096be314b86c2fc3434f2ea32ae3fe4578
shanjws <danishabill9@gmail.com>
2026-09-16 15:12:58 +0700

    Initial commit

---
+++ CombiningTwoConditions/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Choose a class: ");
+string charClass == Console.ReadLine();
+
+if (charClass == "warrior");
+{
+    Console.WriteLine("Skill: Shield Bash");
+}
+else if (charClass == "Mage");
+\`
